2dca4a13260b288365610bd3e1279de471264202
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2020-02-19 21:17:09 +0100

    Metodos de persona implementados

--- persona.cpp
+++ persona.cpp
@@ -1,6 +1,59 @@
 #include "persona.h"
+#include <iostream>
 
+using namespace std;
 Persona::Persona()
 {
+    cout << "No soy nadie" << endl;
+}
 
+Persona::Persona(string nombre, string apellidos, string DNI)
+{
+    this->nombre = nombre;
+    this->apellidos = apellidos;
+    this->DNI = DNI;
+}
+
+Persona::~Persona(void)
+{
+}
+
+string Persona::getClase()
+{
+   if(this->clase==PROFESOR)
+       return "Profesor";
+   else
+       return "Alumno";
+}
+
+string Persona::getNombre()
+{
+    return this->nombre;
+}
+
+string Persona::getApellidos()
+{
+    return this->apellidos;
+}
+
+string Persona::getDNI()
+{
+    return this->DNI;
+}
+
+void Persona::setClase(tipo clase)
+{
+    this->clase = clase;
+}
+void Persona::setNombre(string nombre)
+{
+    this->nombre = nombre;
+}
+void Persona::setApellidos(string apellidos)
+{
+    this->apellidos = apellidos;
+}
+void Persona::setDNI(string DNI)
+{
+    this->DNI = DNI;
 }
