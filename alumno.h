889e1454c9caefb759605c96f11101a7288b3620
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2020-02-19 21:17:17 +0100

    alumno modificado

--- alumno.h
+++ alumno.h
@@ -15,8 +15,6 @@ public:
     int getNumNotas();
     vector<int> getNotas();
 
-    //Setters
-
     //SetNotas metodo sobrecargado (puede tener de 1 a 3 notas)
     void setNotas(int nota1);
     void setNotas(int nota1, int nota2);
