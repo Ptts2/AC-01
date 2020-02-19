f1d6fe684afaabcf90e7c5502ea1a9afad604b58
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2020-02-19 21:33:36 +0100

    Añadidos metodos de alumno

--- alumno.cpp
+++ alumno.cpp
@@ -1,6 +1,39 @@
 #include "alumno.h"
+#include <vector>
+
 
 Alumno::Alumno()
 {
+    this->notas = std::vector<int>();
+}
 
+Alumno::~Alumno()
+{
+}
+
+int Alumno::getNumNotas()
+{
+    return this->notas.size();
+}
+
+vector<int> Alumno::getNotas()
+{
+    return this->notas;
+}
+
+void Alumno::setNotas(int nota1)
+{
+    this->notas.insert(notas.begin(), nota1);
+}
+void Alumno::setNotas(int nota1, int nota2)
+{
+    this->notas.insert(notas.begin(), nota1);
+    this->notas.insert(notas.begin()+1, nota2);
+
+}
+void Alumno::setNotas(int nota1, int nota2, int nota3)
+{
+    this->notas.insert(notas.begin(), nota1);
+    this->notas.insert(notas.begin()+1, nota2);
+    this->notas.insert(notas.begin()+2, nota3);
 }
